11b1436458c8399e3b825a398d005235e1ee9ddd
jumanaalajmi115 <ku.22@hotmail.com>
2020-07-04 17:36:22 +0300

    sp6

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -2,16 +2,35 @@
 
 /*
  * greet(name) اسم الدالة
+ 
  * - name قد تستقبل وقد لا تستقبل
  * - name إن لم تستقبل اسم أو "Hello" تطبع
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
 
-function greet(name) {}
+function greet(name) {
+    if ( name ){
+    
+        {console.lo("hello" + name);}
+    
+    }
+    else 
+   
+    console.log ("hello")}
+greet();
+greet(name);
 
 /*
  * isOdd(n) اسم الدالة
  * - n تستقبل رقم
  * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
  */
-function isOdd(n) {}
+function isOdd(n) {
+if ( n % 2 == 1)
+{return true ;}
+else {
+return false ;
+}
+
+}
+console.log(isOdd(5)) ;
